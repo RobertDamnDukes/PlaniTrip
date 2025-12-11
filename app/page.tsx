9e299690ffbb034403d95354c8cc8a86f776d113
RobertDamnDukes <robbiedukes8@gmail.com>
2025-12-11 09:08:00 -0500

    Added NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function App() {
 
   return (
     <main>
-      <div class="upper-navbar"></div>
+      <div className="upper-navbar"></div>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
