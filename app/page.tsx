61da23a46f8ffe124abcb250254ec9acfa92ca33
RobertDamnDukes <robbiedukes8@gmail.com>
2025-12-30 17:15:13 -0500

    added new button for the location input

--- app/page.tsx
+++ app/page.tsx
@@ -70,6 +70,7 @@ export default function App() {
               <input type="text" id="departureDate" className="dateInput" placeholder="End:  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MMYYYY" />
             </div>
           </div>
+          <button className="sign-up-button search-locations-go"></button>
         </div>
       </div>
       <div className="locations-carousel">
@@ -342,8 +343,86 @@ export default function App() {
               </div>
             </div>
           </div>
+          <div className="card">
+            <div className="content">
+              <div className="back">
+                <div className="back-content">
+                  <div className="location-card-back-image">
+                    <img src="https://www.theinvisibletourist.com/wp-content/uploads/2019/09/featured_122.jpg" alt="Image of Swiss Alps" className="location-card-image-overlay" />
+                    <div className=".location-card-text">
+                      <strong className="location-card-text">Swiss Alps</strong>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="front">
+                <div className="img">
+                  <div className="circle">
+                  </div>
+                  <div className="circle" id="right">
+                  </div>
+                  <div className="circle" id="bottom">
+                  </div>
+                </div>
+
+                <div className="front-content">
+                  <small className="badge">Switzerland</small>
+                  <div className="description">
+                    <div className="title">
+                      <p className="title">
+                        <strong>Zermatt</strong>
+                      </p>
+                      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" /></svg>
+                    </div>
+                    <p className="card-footer">
+                      Mountain Views &nbsp; | &nbsp; Skiing
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div aria-hidden="true" className="group">
+          <div className="card">
+            <div className="content">
+              <div className="back">
+                <div className="back-content">
+                  <div className="location-card-back-image">
+                    <img src="https://www.theinvisibletourist.com/wp-content/uploads/2019/09/featured_122.jpg" alt="Image of Swiss Alps" className="location-card-image-overlay" />
+                    <div className=".location-card-text">
+                      <strong className="location-card-text">Swiss Alps</strong>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="front">
+                <div className="img">
+                  <div className="circle">
+                  </div>
+                  <div className="circle" id="right">
+                  </div>
+                  <div className="circle" id="bottom">
+                  </div>
+                </div>
+
+                <div className="front-content">
+                  <small className="badge">Switzerland</small>
+                  <div className="description">
+                    <div className="title">
+                      <p className="title">
+                        <strong>Zermatt</strong>
+                      </p>
+                      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" /></svg>
+                    </div>
+                    <p className="card-footer">
+                      Mountain Views &nbsp; | &nbsp; Skiing
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="card">
             <div className="content">
               <div className="back">
