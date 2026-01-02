d521d23d98af3edce55a2cc2b913cd253f6d0498
RobertDamnDukes <robbiedukes8@gmail.com>
2026-01-02 17:32:53 -0500

    added react code to try it out, on click for the logo

--- app/page.tsx
+++ app/page.tsx
@@ -13,21 +13,13 @@ Amplify.configure(outputs);
 const client = generateClient<Schema>();
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]); // I think this sets the variables to hold the value of a table row
+  const [logoClickCount, setLogoClickCount] = useState(0);
 
-  function listTodos() { // this is how we pull from our tables. pulls from Todo with the observe query, then subscribes to that table subset
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {  // calls for the dialoge that gets a user input and sends it to the Todo table
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
+  function handleLogoClick() {
+    setLogoClickCount((prev) => {
+      const newCount = prev + 1;
+      console.log("Logo clicked:", newCount);
+      return newCount;
     });
   }
 
@@ -43,11 +35,11 @@ export default function App() {
           <label htmlFor="sidebar-active" className="close-sidebar-button">
             <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#e3e3e3"><path d="m251.33-204.67-46.66-46.66L433.33-480 204.67-708.67l46.66-46.66L480-526.67l228.67-228.66 46.66 46.66L526.67-480l228.66 228.67-46.66 46.66L480-433.33 251.33-204.67Z" /></svg>
           </label>
-          <a>Logo</a>
+          <a onClick={handleLogoClick}>Logo</a>
           <a className="active-link">Planning</a>
           <a>Flights</a>
           <a className="home-link">About</a>
-          <svg className="settings-btn margin-to-right" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="#e8eaed">
+          <svg className="settings-btn marginToRight" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="#e8eaed">
             <path d="m370-80-16-128q-13-5-24.5-12T307-235l-119 50L78-375l103-78q-1-7-1-13.5v-27q0-6.5 1-13.5L78-585l110-190 119 50q11-8 23-15t24-12l16-128h220l16 128q13 5 24.5 12t22.5 15l119-50 110 190-103 78q1 7 1 13.5v27q0 6.5-2 13.5l103 78-110 190-118-50q-11 8-23 15t-24 12L590-80H370Zm70-80h79l14-106q31-8 57.5-23.5T639-327l99 41 39-68-86-65q5-14 7-29.5t2-31.5q0-16-2-31.5t-7-29.5l86-65-39-68-99 42q-22-23-48.5-38.5T533-694l-13-106h-79l-14 106q-31 8-57.5 23.5T321-633l-99-41-39 68 86 64q-5 15-7 30t-2 32q0 16 2 31t7 30l-86 65 39 68 99-42q22 23 48.5 38.5T427-266l13 106Zm42-180q58 0 99-41t41-99q0-58-41-99t-99-41q-59 0-99.5 41T342-480q0 58 40.5 ```html 99t99.5 41Zm-2-140Z"></path>
           </svg>
           <button className="sign-up-button">
@@ -73,7 +65,7 @@ export default function App() {
             </div>
           </div>
           <button className="sign-up-button search-locations-go">
-            Sign up
+            Start Planning
             <div className="arrow-wrapper">
               <div className="arrow"></div>
             </div>
